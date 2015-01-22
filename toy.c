03cc3eaafb12e1035daf2d9397098219c37a88c3
Luis de Bethencourt <luis.bg@samsung.com>
2015-01-22 14:46:09 +0000

    get width and height from sof

--- toy.c
+++ toy.c
@@ -6,7 +6,7 @@ typedef struct
 {
   int width;
   int height;
-} header;
+} sof;                 /* Start of Frame */
 
 typedef struct
 {
@@ -35,6 +35,16 @@ void file_close (fileDesc **f)
   free (*f);
 }
 
+uint8_t read_byte (fileDesc *f, int position)
+{
+  uint8_t ret;
+
+  fseek (f->fp, position, SEEK_SET);
+  fread (&ret, 1, 1, f->fp);
+
+  return ret;
+}
+
 int check_is_jpeg (fileDesc *f)
 {
   uint8_t soi[2] = {0xff, 0xd8};    /* Start Of Image marker */
@@ -43,8 +53,7 @@ int check_is_jpeg (fileDesc *f)
   int i;
 
   for (i = 0; i < 2; i++) {
-    fseek (f->fp, i, SEEK_SET);
-    fread (&r, 1, 1, f->fp);
+    r = read_byte (f, i);
     if (r != soi[i])
       return 0;
   }
@@ -61,6 +70,39 @@ int check_is_jpeg (fileDesc *f)
   return 1;
 }
 
+sof * get_sof (fileDesc *f)
+{
+  sof *ret = (sof *) malloc (sizeof (sof));
+  uint16_t sof[2] = {0xff, 0xc0};
+  uint16_t r0 = 0, r1 = 0;
+  int i = 0, found = 0;
+
+  while (!found) {
+    r0 = read_byte (f, i);
+    if (r0 == sof[0]) {
+      r1 = read_byte (f, i + 1);
+      if (r1 == sof[1])
+        break;    /* i = location of sof */
+    }
+
+    i++;
+  }
+
+  i += 5;    /* in sof, skip segment length and precision */
+  r0 = read_byte (f, i);    /* read height of image */
+  i++;
+  r1 = read_byte (f, i);
+  ret->height = (r0 * 256) + r1;
+
+  i++;
+  r0 = read_byte (f, i);    /* read width of image */
+  i++;
+  r1 = read_byte (f, i);
+  ret->width = (r0 * 256) + r1;
+
+  return ret;
+}
+
 int main(int argc, char *argv[])
 {
   fileDesc *f;
@@ -70,7 +112,15 @@ int main(int argc, char *argv[])
 
   file_start (&f, location);
   printf ("file location: %s\n\n", f->filename);
-  printf ("is it jpeg? %s\n", check_is_jpeg (f)? "yes" : "no");
+
+  if (!check_is_jpeg (f)) {
+    printf ("File is not jpeg\nClosing\n");
+    return 0;
+  }
+
+  sof *s = get_sof (f);
+  printf ("height is %d\n", s->height);
+  printf ("width is %d\n", s->width);
 
   file_close (&f);
   return 0;

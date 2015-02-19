2b37fe66cbd5b71f56724a97e44c742adabf7abb
Luis de Bethencourt <luis.bg@samsung.com>
2015-02-19 19:03:02 +0000

    rename color_components to num_components

--- limp.c
+++ limp.c
@@ -128,7 +128,7 @@ sof * get_sof (fileDesc *f)
   ret->width = (r0 * 256) + r1;
 
   i++;
-  ret->color_components = read_byte (f, i);
+  ret->num_components = read_byte (f, i);
 
   return ret;
 }
@@ -151,7 +151,7 @@ int main(int argc, char *argv[])
   sof *s = get_sof (f);
   printf ("height is %d\n", s->height);
   printf ("width is %d\n", s->width);
-  if (s->color_components == 3)
+  if (s->num_components == 3)
     printf ("color image\n");
   else
     printf ("grayscale image\n");
